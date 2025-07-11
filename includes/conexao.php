bde357fe7d1bd05b007241fc56072c31634a80a2
Alesandro <alesandro.msi.informatica@gmail.com>
2025-07-11 00:28:14 -0300

    Add files via upload

---
+++ includes/conexao.php
@@ -0,0 +1,7 @@
+<?php
+try {
+    $pdo = new PDO("mysql:host=localhost;dbname=loja;charset=utf8mb4", "root", "");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro na conexão: " . $e->getMessage());
+}
